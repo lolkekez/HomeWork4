adcd5516c55939ba1885c6b3ff36952a628c8d79
Oleg Safenreiter <safenreiteroleg14@gmail.com>
2025-04-22 11:19:24 +0300

    add HomeWork5

---
+++ src/test/java/HomeWork5.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.*;
+import net.bytebuddy.build.Plugin;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class HomeWork5 {
+    @BeforeAll
+    static void prepareEnvironment() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void gitEnterprisesTest() {
+        open("https://github.com");
+        $(byText("Solutions")).hover();
+        $(byTagAndText("a", "Enterprises")).click();
+        $(byTagAndText("h1", "The AI-powered developer platform"));
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+        Selenide.actions()
+                .clickAndHold($("#column-a"))
+                .moveToElement($("#column-b"))
+                .release()
+                .perform();
+
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+    }
+}
